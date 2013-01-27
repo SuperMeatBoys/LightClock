2196a1056a9aaa959008e7db54d1be0a604d1dfb
gesuper <gejianghua100@163.com>
2013-01-27 20:35:11 +0800

    remove unused improt

--- src/com/gesuper/lightclock/view/AlertListView.java
+++ src/com/gesuper/lightclock/view/AlertListView.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
